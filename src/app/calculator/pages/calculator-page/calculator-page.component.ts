7354e502bc4e2cde806fdff3fb5d4d39265ae164
Santiago Reyes <santiago.reyes@platinum.net.co>
2025-10-22 09:05:08 -0500

    Inicio curso pro de angular primera seccion zoneless calculator

---
+++ src/app/calculator/pages/calculator-page/calculator-page.component.ts
@@ -0,0 +1,12 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+
+@Component({
+  selector: 'calculator-page',
+  imports: [],
+  templateUrl: './calculator-page.component.html',
+  styleUrl: './calculator-page.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export default class CalculatorPageComponent {
+
+}
