f3f0a218a5bb88eac55c036dc5ff72246466c84a
Santiago Reyes <santiago.reyes@platinum.net.co>
2025-10-27 09:18:28 -0500

    Terminando seccion 3 - entendimiento de los host y viewChildren

--- src/app/calculator/components/calculator-button/calculator-button.ts
+++ src/app/calculator/components/calculator-button/calculator-button.ts
@@ -1,4 +1,13 @@
-import {ChangeDetectionStrategy, Component, HostBinding, input} from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  HostBinding,
+  input,
+  output,
+  signal,
+  viewChild
+} from '@angular/core';
 
 @Component({
   selector: 'calculator-button',
@@ -7,12 +16,30 @@ import {ChangeDetectionStrategy, Component, HostBinding, input} from '@angular/c
   styleUrl: './calculator-button.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
-    class: "border-r border-b border-indigo-400"
+    class: `border-r border-b border-indigo-400`,
+    '[class]': `size()`
   },
   // encapsulation: ViewEncapsulation.None
 })
 export class CalculatorButton {
-  isOperationButton = input<boolean>(false)
+  size= input<string>('grid-spacing-1');
+  isOperationButton = input<boolean>(false);
+  contentValue = viewChild<ElementRef<HTMLButtonElement>>('button');
+  onClick = output<string>();
+  isPressed = signal<boolean>(false);
+  protected handleClick() {
+    const value = this.contentValue()?.nativeElement.innerText.trim();
+    if (!value) return;
+
+    this.onClick.emit(value);
+  }
+  keyboardPressedStyle(key: string){
+    if (!this.contentValue()) return;
+    const value = this.contentValue()?.nativeElement.innerText.trim();
+    if (value !== key) return;
+    this.isPressed.set(true);
+    setTimeout(()=> this.isPressed.set(false),100)
+  }
   // isOperationButton = input(false,
   //   {transform: (value: string|boolean)=> typeof value === 'string' ? value === '': value}
   // )
--- src/app/calculator/components/calculator/calculator.ts
+++ src/app/calculator/components/calculator/calculator.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, HostListener, viewChildren} from '@angular/core';
 import {CalculatorButton} from '@calculator/components/calculator-button/calculator-button';
 
 interface CalculatorButtons {
@@ -13,29 +13,56 @@ interface CalculatorButtons {
     CalculatorButton
   ],
   templateUrl: './calculator.html',
-  styleUrl: './calculator.css'
+  standalone: true,
+  styleUrl: './calculator.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '(document:keyup)': 'handleKeyboardEvent($event)'
+  }
 })
 export class Calculator {
-  protected buttons:CalculatorButtons[] =
+  Calculatorbuttons = viewChildren(CalculatorButton);
+  protected buttons: CalculatorButtons[] =
     [
-      {value:'C', isOperation:true, size: 1},
-      {value:'+/-',isOperation: true, size: 1},
-      {value:'%',isOperation: true, size: 1},
-      {value:'÷',isOperation: true, size: 1},
-      {value:'7',isOperation: false, size: 1},
-      {value:'8',isOperation: false, size: 1},
-      {value:'9',isOperation: false, size: 1},
-      {value:'x',isOperation: true, size: 1},
-      {value:'4',isOperation: false, size: 1},
-      {value:'5',isOperation: false, size: 1},
-      {value:'6',isOperation: false, size: 1},
-      {value:'-',isOperation: true, size: 1},
-      {value:'1',isOperation: false, size: 1},
-      {value:'2',isOperation: false, size: 1},
-      {value:'3',isOperation: false, size: 1},
-      {value:'+',isOperation: true, size: 1},
-      {value:'0',isOperation: false, size: 1},
-      {value:'.',isOperation: true, size: 1},
-      {value:'=',isOperation: true, size: 2}
+      {value: 'C', isOperation: true, size: 1},
+      {value: '+/-', isOperation: true, size: 1},
+      {value: '%', isOperation: true, size: 1},
+      {value: '÷', isOperation: true, size: 1},
+      {value: '7', isOperation: false, size: 1},
+      {value: '8', isOperation: false, size: 1},
+      {value: '9', isOperation: false, size: 1},
+      {value: 'x', isOperation: true, size: 1},
+      {value: '4', isOperation: false, size: 1},
+      {value: '5', isOperation: false, size: 1},
+      {value: '6', isOperation: false, size: 1},
+      {value: '-', isOperation: true, size: 1},
+      {value: '1', isOperation: false, size: 1},
+      {value: '2', isOperation: false, size: 1},
+      {value: '3', isOperation: false, size: 1},
+      {value: '+', isOperation: true, size: 1},
+      {value: '0', isOperation: false, size: 1},
+      {value: '.', isOperation: true, size: 1},
+      {value: '=', isOperation: true, size: 2}
     ];
+
+  handleClickEvent(key: string) {
+    console.log({key})
+  }
+
+  // @HostListener('document:keyup',['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    // console.log(event,event.key)
+    const keyEquivalents: Record<string, string> = {
+      Escape: 'C',
+      Clear: 'C',
+      Enter: '=',
+      '/': '÷',
+      '*': 'x'
+    }
+    const actualValue = keyEquivalents[event.key] ? keyEquivalents[event.key]: event.key
+    this.handleClickEvent(actualValue)
+    this.Calculatorbuttons().forEach(button => {
+      button.keyboardPressedStyle(actualValue)
+    })
+  }
 }
